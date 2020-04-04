741718dfef0302f1dff1d0e9b795471a10c170c2
jahartley <52391697+jahartley@users.noreply.github.com>
2020-04-04 16:52:20 -0400

    update

--- ezoCal.js
+++ ezoCal.js
@@ -1,2 +1,12 @@
 // EZO calibration program.
 // must have other i2c programs stopped.
+
+const prompt = require('prompt-sync')({sigint: true});
+const i2c = require('i2c-bus');
+
+//variables
+var i2cAddress = 0;
+
+console.log("welcome to ezo cal.");
+i2cAddress = prompt('enter i2c address');
+console.log(`you entered ${i2cAddress}`);
