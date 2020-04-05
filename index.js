4d19d58e617ca6097a20179e0d6a8490bfa0690c
jahartley <52391697+jahartley@users.noreply.github.com>
2020-04-05 16:14:05 -0400

    add bme680

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://192.168.77.1')
 const { Bme680 } = require('bme680-sensor')
 const bme680 = new Bme680(1, 0x77)//pool Pi main program
+
+client.on('message', function(topic, message) {
+  console.log(message.toString())
+})
+
+
+bme680.initialize().then(async () => {
+  console.log('Sensor Init')
+  setInterval(async () => {
+    bme680result =  await bme680.getSensorData()
+    console.log(bme680result)
+    client.publish('home/pool/airTemp1', bme680result.data.temperature.toString())
+    client.publish('home/pool/airPress1', bme680result.data.pressure.toString())
+    client.publish('home/pool/airHumid1', bme680result.data.humidity.toString())
+  }, 120000);
+});
