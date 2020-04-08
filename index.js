ba108a9af8378c311d91f7ecce58c7854b0a6dc3
jahartley <52391697+jahartley@users.noreply.github.com>
2020-04-08 00:02:34 -0400

    add ds18b20 check

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://192.168.77.1')
 const { Bme680 } = require('bme680-sensor')
 const bme680 = new Bme680(1, 0x77)//pool Pi main program
+const DS18B20 = require('@jahartley/ds2482-temperature');
+const sense = new DS18B20();
+
+
 
 client.on('message', function(topic, message) {
   console.log(message.toString())
@@ -26,3 +30,20 @@ bme680.initialize().then(async () => {
     if (bme680result.data.heat_stable == true) client.publish('home/pool/airQuality1', bme680result.data.gas_resistance.toString());
   }, 120000);
 });
+
+sense.init().then(() => {
+  console.log('ds18b20 init')
+  sense.search()}).then(() => {
+    setInterval(async () => {
+      sense.readTemperatures().then(temps => {
+        console.log(temps); // Returns a list of temperature reading from all found sensors
+        console.log('first temp');
+        console.log(temps[0]);
+      })  
+    }, 120000)
+  })
+.catch(err => {
+  console.error(err);
+});
+  
+
